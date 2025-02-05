d8f49beac897891ed41af2e19a5f8e847c5299fc
Nuno <76601899+nnunodev@users.noreply.github.com>
2025-02-05 12:13:59 +0000

    Spring boot setup added

---
+++ src/main/java/com/gameshelf/gameshelf/GameShelfApplication.java
@@ -0,0 +1,13 @@
+package com.gameshelf.gameshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameShelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameShelfApplication.class, args);
+	}
+
+}
