e8783486a31634d6449799725323c569684bd8ff
Nuno <76601899+nnunodev@users.noreply.github.com>
2025-02-06 00:57:21 +0000

    updated flyway

--- src/test/java/com/gameshelf/repository/UserRepositoryTest.java
+++ src/test/java/com/gameshelf/repository/UserRepositoryTest.java
@@ -1,4 +1,53 @@
 package com.gameshelf.repository;
 
-public class UserRepositoryTest {
+import com.gameshelf.model.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+import java.util.Set;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void testSaveUser() {
+        // Arrange
+        User user = new User(null, "testuser", "test@example.com", "password", Set.of("USER"));
+
+        // Act
+
+        User savedUser = userRepository.save(user);
+
+        // Assert
+        assertThat(savedUser.getId()).isNotNull();
+        assertThat(userRepository.findByUsername("testuser")).isPresent();
+        assertThat(userRepository.findByEmail("test@example.com")).isPresent();
+    }
+
+    @Test
+    void testFindByUsername() {
+        // Arrange
+        User user = new User(null, "findtestuser", "test@example.com", "password", Set.of("USER"));
+        userRepository.save(user);
+        // Act
+        Optional<User> foundUser = userRepository.findByUsername("findtestuser");
+        // Assert
+        assertThat(foundUser).isPresent();
+        assertThat(foundUser.get().getUsername()).isEqualTo("findtestuser");
+    }
+    @Test
+    void testFindByEmail() {
+        // Arrange
+        User user = new User(null, "findtestuser", "test@example.com", "password", Set.of("USER"));
+        userRepository.save(user);
+        // Act
+        Optional<User> foundUser = userRepository.findByEmail("test@example.com");
+        // Assert
+        assertThat(foundUser.get().getEmail()).isEqualTo("test@example.com");
+    }
 }
